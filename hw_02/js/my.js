df728309f2483e8bdaae90e7bcce28de90f43865
asnvir <asnvir@gmail.com>
2022-07-09 13:01:24 +0300

    HomeWork - 2 (after changes)

--- hw_02/js/my.js
+++ hw_02/js/my.js
@@ -1,22 +1,24 @@
 const CURRENT_YEAR = 2022;
 
-let name = prompt("Enter you name.");
-let lastName = prompt("Enter your lastname.");
-let birthYear = Number(prompt("Enter your birth year."));
-let age = CURRENT_YEAR - birthYear;
+const name = prompt("Enter you name.");
+const lastName = prompt("Enter your lastname.");
+const birthYear = Number(prompt("Enter your birth year."));
+const age = CURRENT_YEAR - birthYear;
 
-if (name !== null && name !== "") {
+if (name) {
     document.getElementById("name").innerHTML = "Name: " + name;
-}else {
+} else {
     document.getElementById("name").innerHTML = "You need reenter your name.";
 }
-if (lastName !== null && lastName !== "") {
+
+if (lastName) {
     document.getElementById("lastName").innerHTML = "Last name: " + lastName;
-}else{
+} else {
     document.getElementById("lastName").innerHTML = "You need reenter your last name.";
 }
-if (birthYear !== null && age !== 2022) {
+
+if (birthYear) {
     document.getElementById("age").innerHTML = "Age: " + age;
-}else{
+} else {
     document.getElementById("age").innerHTML = "You need reenter your birth year";
 }
